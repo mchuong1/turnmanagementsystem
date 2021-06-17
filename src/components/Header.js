7d850f408afa9574dd19fe8bdce81976fe5b248a
Matthew Chuong <mstchuong@yahoo.com>
2021-06-17 14:42:11 -0400

    Remove unused imports
    - update react tab name

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,6 @@ import {
   AppBar, Toolbar, Typography
 } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles';
-import LoginButton from './LoginButton';
-import LogoutButton from './LogoutButton';
-import Profile from './Profile';
 
 const useStyles = makeStyles({
   primary: {
