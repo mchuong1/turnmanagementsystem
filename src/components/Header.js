d1f9f94ff26a93e95d53b83e7b79dea7a41d337e
Matthew Chuong <mstchuong@yahoo.com>
2021-06-17 13:50:03 -0400

    Create Login, Logout, and Profile Components
    - Setup Auth0

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,9 @@ import {
   AppBar, Toolbar, Typography
 } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles';
+import LoginButton from './LoginButton';
+import LogoutButton from './LogoutButton';
+import Profile from './Profile';
 
 const useStyles = makeStyles({
   primary: {
@@ -19,9 +22,9 @@ export default function Header() {
   return (
     <AppBar position="static">
       <Toolbar variant="dense" classes={{root: classes.primary}}>
-      <Typography variant="h6" color="inherit">
-        Polish Nail Salon
-      </Typography>
+        <Typography variant="h6" color="inherit">
+          Polish Nail Salon
+        </Typography>
       </Toolbar>
     </AppBar>
   )
