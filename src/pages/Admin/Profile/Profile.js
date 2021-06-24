903a721cdd54183c42b7191826d7f4ae35d9cc92
Matthew Chuong <mstchuong@yahoo.com>
2021-06-23 21:08:48 -0400

    Move Profile to Admin folder

--- src/pages/Admin/Profile/Profile.js
+++ src/pages/Admin/Profile/Profile.js
@@ -1,13 +1,8 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import LogoutButton from "./LogoutButton";
 
 const Profile = () => {
-  const { user, isAuthenticated, isLoading } = useAuth0();
-
-  if (isLoading) {
-    return <div>Loading ...</div>;
-  }
+  const { user, isAuthenticated } = useAuth0();
 
   return (
     isAuthenticated && (
@@ -15,7 +10,6 @@ const Profile = () => {
         <img src={user.picture} alt={user.name} />
         <h2>{user.name}</h2>
         <p>{user.email}</p>
-        <LogoutButton />
       </div>
     )
   );
