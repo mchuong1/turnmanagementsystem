f51aa64799138859aca0a237c2a04776ffe3f18f
Matthew Chuong <mstchuong@yahoo.com>
2021-06-23 21:02:36 -0400

    center the loading circle

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { Redirect, Route } from 'react-router';
 import { useAuth0 } from "@auth0/auth0-react";
-import { CircularProgress } from '@material-ui/core';
+import { CircularProgress, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  root: {
+    display: 'grid',
+    placeItems: 'center'
+  }
+})
 
 export default function PrivateRoute({ component:Component, ...rest }) {
 
+  const classes = useStyles();
   const { isAuthenticated, isLoading, user } = useAuth0();
 
   if (isLoading) {
     return (
-      <div>
+      <div className={classes.root}>
         <CircularProgress />
         Loading...
       </div>
