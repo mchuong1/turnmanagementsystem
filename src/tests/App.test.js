625b5ff9eabee6b96ee2f9be951d5d51993b4ff9
Matthew Chuong <mstchuong@yahoo.com>
2021-07-02 17:07:23 -0400

    lint

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,17 +3,11 @@ import React from 'react'
 import { mount } from 'enzyme'
 import App from '../App'
 import { findByTestAttr } from '../../test/testUtils'
-import BrowserRouter from 'react-router'
 
 const setup = () => {
   return mount(<App />)
 }
 describe('it renders', () => {
-  const mockUseRouteMatch = {
-    path: '',
-    location: ''
-  }
-
   test('renders without crashing', () => {
     const wrapper = setup()
     const app = findByTestAttr(wrapper, 'component-app')
