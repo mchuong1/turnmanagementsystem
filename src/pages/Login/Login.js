a2f9b7d9def270a70981f95c00fbe10be2e2f626
Matthew Chuong <mstchuong@yahoo.com>
2021-07-03 10:47:08 -0400

    Have the Auth0 Login as default page

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,19 +1,29 @@
-import { makeStyles } from '@material-ui/core'
-import React from 'react'
-import LoginButton from './LoginButton'
+import { useAuth0 } from '@auth0/auth0-react';
+import { makeStyles, Button } from '@material-ui/core';
+import React from 'react';
 
 const useStyles = makeStyles({
   body: {
-    height: '100%'
-  }
-})
+    height: '100%',
+    display: 'grid',
+    placeItems: 'center',
+  },
+});
 
-export default function Login () {
+export default function Login() {
   const classes = useStyles();
+
+  const { loginWithRedirect } = useAuth0();
+
   return (
-    <div className={classes.body} data-test="component-login">
-      Login
-      <LoginButton />
+    <div className={classes.body} data-test='component-login'>
+      <Button
+        variant='contained'
+        color='primary'
+        onClick={() => loginWithRedirect()}
+      >
+        Login
+      </Button>
     </div>
-  )
+  );
 }
